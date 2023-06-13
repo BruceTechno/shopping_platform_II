f5314d9a4d2e8339113de6f0ad9305a5d6db8223
Senhao0905 <senhao0905@outlook.com>
2023-06-13 09:45:42 +0800

    update userimpl

--- src/main/java/com/example/shopping_platform_II/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/shopping_platform_II/service/impl/UserServiceImpl.java
@@ -14,158 +14,172 @@ import javax.servlet.http.HttpSession;
 
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserDao userDao;
+	@Autowired
+	private UserDao userDao;
 
-    @Override
-    public RegisterResponse register(RegisterRequest request) {
-        String account = request.getAccount();
-        String pwd = request.getPwd();
-        String name = request.getName();
-        String phone = request.getPhone();
-        String address = request.getAddress();
-        String patternPwd = "^(?=.+[\\p{Punct}])(?!.*[\\s\\t\\r\\n\\f])[\\p{Print}]{8,12}$";//8~12碼 至少包含一個特殊符號
-        if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd) || !StringUtils.hasText(name) ||
-                !StringUtils.hasText(phone) || !StringUtils.hasText(address)) {
-            return new RegisterResponse(RtnCode.DATA_ERROR.getMessage());
-        }
-        if(!phone.matches("[0-9]{4}-[0-9]{6}") || !pwd.matches(patternPwd)){
-            return new RegisterResponse(RtnCode.DATA_ERROR.getMessage());
-        }
+	@Override
+	public RegisterResponse register(RegisterRequest request) {
+		String account = request.getAccount();
+		String pwd = request.getPwd();
+		String name = request.getName();
+		String phone = request.getPhone();
+		String address = request.getAddress();
+		String patternPwd = "^(?=.+[\\p{Punct}])(?!.*[\\s\\t\\r\\n\\f])[\\p{Print}]{8,12}$";// 8~12碼 至少包含一個特殊符號
+		if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd) || !StringUtils.hasText(name)
+				|| !StringUtils.hasText(phone) || !StringUtils.hasText(address)) {
+			return new RegisterResponse(RtnCode.DATA_ERROR.getMessage());
+		}
+		if (!phone.matches("[0-9]{4}-[0-9]{6}") || !pwd.matches(patternPwd)) {
+			return new RegisterResponse(RtnCode.DATA_ERROR.getMessage());
+		}
 
-        int result = userDao.insertUserInfoWhereNotExists(account, pwd, name, address, phone);
+		int result = userDao.insertUserInfoWhereNotExists(account, pwd, name, address, phone);
 
-        if (result == 0) {
-            return new RegisterResponse(RtnCode.DATA_DUPLICATE.getMessage());
-        }
-        return new RegisterResponse(RtnCode.SUCCESSFUL.getMessage());
-    }
+		if (result == 0) {
+			return new RegisterResponse(RtnCode.DATA_DUPLICATE.getMessage());
+		}
+		return new RegisterResponse(RtnCode.SUCCESSFUL.getMessage());
+	}
 
-    @Override
-    public LoginResponse active(LoginRequest request) {
-        String account = request.getAccount();
-        String pwd = request.getPwd();
-        if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
-            return new LoginResponse(RtnCode.CANNOT_EMPTY.getMessage());
-        }
-        User result = userDao.findByAccountAndPwd(account, pwd);
-        if (result == null) {
-            return new LoginResponse(RtnCode.DATA_ERROR.getMessage());
-        }
-        result.setActive(true);
-        userDao.save(result);
-        return new LoginResponse(RtnCode.SUCCESSFUL.getMessage(), result);
-    }
+	@Override
+	public LoginResponse active(LoginRequest request) {
+		String account = request.getAccount();
+		String pwd = request.getPwd();
+		if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
+			return new LoginResponse(RtnCode.CANNOT_EMPTY.getMessage());
+		}
+		User result = userDao.findByAccountAndPwd(account, pwd);
+		if (result == null) {
+			return new LoginResponse(RtnCode.DATA_ERROR.getMessage());
+		}
+		result.setActive(true);
+		userDao.save(result);
+		return new LoginResponse(RtnCode.SUCCESSFUL.getMessage(), result);
+	}
 
-    @Override
-    public LoginResponse login(LoginRequest request) {//todo ++ Session done
+	@Override
+	public LoginResponse login(LoginRequest request) {// todo ++ Session done
 
-        String account = request.getAccount();
-        String pwd = request.getPwd();
+		String account = request.getAccount();
+		String pwd = request.getPwd();
 
-        User result = userDao.findByAccountAndPwdAndActive(account, pwd, true);
-        if (result == null) {
-            return new LoginResponse(RtnCode.DATA_ERROR.getMessage());//登入失敗或未激活
-        }
-        return new LoginResponse(RtnCode.SUCCESSFUL.getMessage());
-    }
+		User result = userDao.findByAccountAndPwdAndActive(account, pwd, true);
+		if (result == null) {
+			return new LoginResponse(RtnCode.DATA_ERROR.getMessage());// 登入失敗或未激活
+		}
+		return new LoginResponse(RtnCode.SUCCESSFUL.getMessage());
+	}
 
-    @Override
-    public UpdateResponse updatePwd(HttpSession session , UpdateRequest request) {
-        String account = (String) session.getAttribute("account");
-        String pwd = (String) session.getAttribute("pwd");
-        String newPwd = request.getPwd();
-        String patternPwd = "^(?=.+[\\p{Punct}])(?!.*[\\s\\t\\r\\n\\f])[\\p{Print}]{8,12}$";//8~12碼 至少包含一個特殊符號
+	@Override
+	public UpdateResponse updatePwd(HttpSession session, UpdateRequest request) {
+		String account = (String) session.getAttribute("account");
+		String pwd = (String) session.getAttribute("pwd");
+		String newPwd = request.getPwd();
+		String patternPwd = "^(?=.+[\\p{Punct}])(?!.*[\\s\\t\\r\\n\\f])[\\p{Print}]{8,12}$";// 8~12碼 至少包含一個特殊符號
 
+		if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
+			return new UpdateResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
+		}
+		if (!StringUtils.hasText(newPwd)) {
+			return new UpdateResponse(RtnCode.CANNOT_EMPTY.getMessage());
+		}
+		if (!newPwd.matches(patternPwd)) {
+			return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
+		}
+		int result = userDao.updatePwdByAccount(newPwd, account);
+		if (result == 0) {
+			return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
+		}
+		return new UpdateResponse(RtnCode.SUCCESSFUL.getMessage());
+	}
 
-        if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
-            return new UpdateResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
-        }
-        if (!StringUtils.hasText(newPwd)) {
-            return new UpdateResponse(RtnCode.CANNOT_EMPTY.getMessage());
-        }
-        if(!newPwd.matches(patternPwd)){
-            return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
-        }
-        int result = userDao.updatePwdByAccount(newPwd, account);
-        if (result == 0) {
-            return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
-        }
-        return new UpdateResponse(RtnCode.SUCCESSFUL.getMessage());
-    }
+	@Override
+	public UpdateResponse updateName(HttpSession session, UpdateRequest request) {// todo name符合正規才能update 阿要符合啥規
+		String account = (String) session.getAttribute("account");
+		String pwd = (String) session.getAttribute("pwd");
+		String name = request.getName();
 
-    @Override
-    public UpdateResponse updateName(HttpSession session ,UpdateRequest request) {//todo name符合正規才能update 阿要符合啥規
-        String account = (String) session.getAttribute("account");
-        String pwd = (String) session.getAttribute("pwd");
-        String name = request.getName();
+		if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
+			return new UpdateResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
+		}
+		if (!StringUtils.hasText(name)) {
+			return new UpdateResponse(RtnCode.CANNOT_EMPTY.getMessage());
+		}
+		int result = userDao.updateNameByAccount(name, account);
+		if (result == 0) {
+			return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
+		}
+		return new UpdateResponse(RtnCode.SUCCESSFUL.getMessage());
+	}
 
+	@Override
+	public UpdateResponse updateAddress(HttpSession session, UpdateRequest request) {
+		String account = (String) session.getAttribute("account");
+		String pwd = (String) session.getAttribute("pwd");
 
-        if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
-            return new UpdateResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
-        }
-        if (!StringUtils.hasText(name)) {
-            return new UpdateResponse(RtnCode.CANNOT_EMPTY.getMessage());
-        }
-        int result = userDao.updateNameByAccount(name, account);
-        if (result == 0) {
-            return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
-        }
-        return new UpdateResponse(RtnCode.SUCCESSFUL.getMessage());
-    }
+		String address = request.getAddress();
+		if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
+			return new UpdateResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
+		}
+		if (!StringUtils.hasText(address)) {
+			return new UpdateResponse(RtnCode.CANNOT_EMPTY.getMessage());
+		}
+		int result = userDao.updateAddressByAccount(address, account);
+		if (result == 0) {
+			return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
+		}
+		return new UpdateResponse(RtnCode.SUCCESSFUL.getMessage());
+	}
 
-    @Override
-    public UpdateResponse updateAddress(HttpSession session ,UpdateRequest request) {
-        String account = (String) session.getAttribute("account");
-        String pwd = (String) session.getAttribute("pwd");
+	@Override
+	public UpdateResponse updatePhone(HttpSession session, UpdateRequest request) {// todo phone符合正規才能update done
+		String account = (String) session.getAttribute("account");
+		String pwd = (String) session.getAttribute("pwd");
+		String phone = request.getPhone();
 
-        String address = request.getAddress();
-        if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
-            return new UpdateResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
-        }
-        if (!StringUtils.hasText(address)) {
-            return new UpdateResponse(RtnCode.CANNOT_EMPTY.getMessage());
-        }
-        int result = userDao.updateAddressByAccount(address, account);
-        if (result == 0) {
-            return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
-        }
-        return new UpdateResponse(RtnCode.SUCCESSFUL.getMessage());
-    }
+		if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
+			return new UpdateResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
+		}
+		if (!StringUtils.hasText(phone)) {
+			return new UpdateResponse(RtnCode.CANNOT_EMPTY.getMessage());
+		}
+		if (!phone.matches("[0-9]{4}-[0-9]{6}")) {
+			return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
+		}
+		int result = userDao.updatePhoneByAccount(phone, account);
+		if (result == 0) {
+			return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
+		}
+		return new UpdateResponse(RtnCode.SUCCESSFUL.getMessage());
+	}
 
-    @Override
-    public UpdateResponse updatePhone(HttpSession session ,UpdateRequest request) {//todo phone符合正規才能update done
-        String account = (String) session.getAttribute("account");
-        String pwd = (String) session.getAttribute("pwd");
-        String phone = request.getPhone();
+	@Override
+	public LoginResponse logout(HttpSession session) {
+		String account = (String) session.getAttribute("account");
+		String pwd = (String) session.getAttribute("pwd");
 
-        if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
-            return new UpdateResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
-        }
-        if (!StringUtils.hasText(phone)) {
-            return new UpdateResponse(RtnCode.CANNOT_EMPTY.getMessage());
-        }
-        if(!phone.matches("[0-9]{4}-[0-9]{6}")){
-            return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
-        }
-        int result = userDao.updateAddressByAccount(phone, account);
-        if (result == 0) {
-            return new UpdateResponse(RtnCode.DATA_ERROR.getMessage());
-        }
-        return new UpdateResponse(RtnCode.SUCCESSFUL.getMessage());
-    }
+		if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
+			return new LoginResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
+		}
+		session.removeAttribute("account");
+		session.removeAttribute("pwd");
+		return new LoginResponse(RtnCode.SUCCESSFUL.getMessage());
+	}
 
-    @Override
-    public LoginResponse logout(HttpSession session ) {
-        String account = (String) session.getAttribute("account");
-        String pwd = (String) session.getAttribute("pwd");
+	@Override
+	public GetUserResponse getUserInfoGetUserResponse(HttpSession session) {
+		// TODO Auto-generated method stub
+		String account = (String) session.getAttribute("account");
+		String pwd = (String) session.getAttribute("pwd");
 
-        if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
-            return new LoginResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
-        }
-        session.removeAttribute("account");
-        session.removeAttribute("pwd");
-        return new LoginResponse(RtnCode.SUCCESSFUL.getMessage());
-    }
+		if (!StringUtils.hasText(account) || !StringUtils.hasText(pwd)) {
+			return new GetUserResponse(RtnCode.PLEASE_LOGIN_FIRST.getMessage());
+		}
+
+		User res = userDao.findByAccountAndPwd(account, pwd);
+		
+		
+		
+		return new GetUserResponse(RtnCode.SUCCESSFUL.getMessage(), res);
+	}
 }
-
